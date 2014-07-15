5cb6c651db7be1da981b8fbab19bc63ca385d9c5
David García <davidgf1987@gmail.com>
2014-07-15 10:27:32 +0200

    Mysql2 gem added

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :web, %w{deploy@myeexample.com}
-role :app, %w{deploy@myeexample.com}
-role :db,  %w{deploy@myeexample.com}
+role :web, %w{root@diegoysandra.com}
+role :app, %w{root@diegoysandra.com}
+role :db,  %w{root@diegoysandra.com}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@myeexample.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'diegoysandra.com', user: 'root', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
