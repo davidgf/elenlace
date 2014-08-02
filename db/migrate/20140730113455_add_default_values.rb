222ecb85fe6d89400482fed69b759b968413974b
David García <davidgf1987@gmail.com>
2014-08-02 12:24:17 +0200

    Migration fix

--- db/migrate/20140730113455_add_default_values.rb
+++ db/migrate/20140730113455_add_default_values.rb
@@ -1,7 +1,13 @@
 class AddDefaultValues < ActiveRecord::Migration
-  def change
+  def up
+    Attendee.where(description: nil).update_all(description: "")
+    Message.where(message: nil).update_all(message: "")
+    Picture.where(name: nil).update_all(name: "")
   	change_column :attendees, :description, :text, :null => false, :default => ""
   	change_column :messages, :message, :text, :null => false, :default => ""
-  	change_column :pictures, :name, :string, :null => false, :default => ""
+  	# change_column :pictures, :name, :string, :null => false, :default => ""
+  end
+
+  def down
   end
 end
