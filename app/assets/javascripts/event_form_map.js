fa2e17d3c4148a92376e9ddac0d7022cd5cdd023
David García <davidgf1987@gmail.com>
2014-12-18 16:09:47 +0100

    Events: marker geolocation

--- app/assets/javascripts/event_form_map.js
+++ app/assets/javascripts/event_form_map.js
@@ -1,14 +1,70 @@
 $(function(){
-    var mapCenter = new google.maps.LatLng(40.41331,-3.69392);
+
+    var $eventLatInput = $('#event_location_attributes_lat');
+    var $eventLongInput = $('#event_location_attributes_long');
+    var eventLat = $eventLatInput.val();
+    var eventLong = $eventLongInput.val();
+    var mapCenter;
+    var zoom;
+    if(eventLat.length > 0 && eventLong.length > 0){
+        mapCenter = new google.maps.LatLng(eventLat, eventLong);
+        zoom = 15;
+    } else {
+        mapCenter = new google.maps.LatLng(40.41331,-3.69392);
+        zoom = 6;
+    }
+
     var mapOptions = {
       center: mapCenter,
-      zoom: 6
+      zoom: zoom
     };
     var map = new google.maps.Map($('#map-canvas')[0], mapOptions);
 
     var marker = new google.maps.Marker({
         position: mapCenter,
         map: map,
-        draggable:true
+        draggable: true
+    });
+
+    var geocoder = new google.maps.Geocoder();
+
+    function codeAddress(address) {
+        geocoder.geocode( { 'address': address}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            map.setZoom(15);
+            marker.setPosition(results[0].geometry.location);
+          } else {
+            alert("No hemos podido encontrar la dirección proporcionada");
+          }
+        });
+    }
+
+    function onAddressRequest(ev){
+        ev.preventDefault();
+        var address = $addressInput.val();
+        codeAddress(address);
+    }
+
+    var $addressInput = $('#event_location_attributes_address');
+
+    $addressInput.keypress(function(event) {
+        if (event.which == 13) {
+            onAddressRequest(event);
+        }
+    });
+
+    var $searchAddressBtn = $('#searchAddress');
+
+    $searchAddressBtn.on('click', function(event) {
+        onAddressRequest(event);
+    })
+
+    google.maps.event.addListener(marker, 'position_changed', function() {
+        var markerPos = marker.getPosition();
+        var markerLat = markerPos.lat();
+        var markerLong = markerPos.lng();
+        $eventLatInput.val(markerLat);
+        $eventLongInput.val(markerLong);
     });
 })
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -75,6 +75,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:datetime, :description, :name, location_attributes: [:name, :address])
+      params.require(:event).permit(:datetime, :description, :name, location_attributes: [:name, :address, :lat, :long])
     end
 end
