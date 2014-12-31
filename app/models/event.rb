ae897e929a60b2fbce5405b68d7a694f83b85da2
David García <davidgf1987@gmail.com>
2014-12-31 10:37:09 +0100

    Event missing image

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,6 @@ class Event < ActiveRecord::Base
 	belongs_to :wedding
     has_one :location
 	accepts_nested_attributes_for :location
-    has_attached_file :image, :styles => { :big => "1200x1200>", :medium => "500x500>", :thumb => "100x100>" }, :convert_options => { :all => '-auto-orient' }, :default_url => "/images/:style/missing.png"
+    has_attached_file :image, :styles => { :big => "1200x1200>", :medium => "500x500>", :thumb => "100x100>" }, :convert_options => { :all => '-auto-orient' }, :default_url => "/images/:style/event_missing_image.png"
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
